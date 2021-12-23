9e3172a5c14997e0cf3422c6746d7f4e1f9fc902
desiraes98597 <desiraes@gmail.com>
2021-12-22 20:35:18 -0800

    js file updated with comment

--- js/checkout.js
+++ js/checkout.js
@@ -7,6 +7,8 @@ alert('Your total adoption fees are currently '+adoptFee);
 document.getElementById("finalAmount").innerHTML = adoptFee = Math.round((adoptFee+ Number.EPSILON) * 100) / 100;
 }
 
+//  Jquery code attempt
+
 // $(runningTotal(current));
 //  {
 //     adoptFee = Math.round((adoptFee+current+ Number.EPSILON) * 100) / 100;        
