cd667fc3b8929124dc4e021f332ff71c7ebc77a9
desiraes98597 <desiraes@gmail.com>
2021-12-22 20:30:02 -0800

    js file modiflied with error code

--- js/checkout.js
+++ js/checkout.js
@@ -5,11 +5,21 @@ function runningTotal(current) {
 adoptFee = Math.round((adoptFee+current+ Number.EPSILON) * 100) / 100;        
 alert('Your total adoption fees are currently '+adoptFee);
 document.getElementById("finalAmount").innerHTML = adoptFee = Math.round((adoptFee+ Number.EPSILON) * 100) / 100;
+}
+
+// $(runningTotal(current));
+//  {
+//     adoptFee = Math.round((adoptFee+current+ Number.EPSILON) * 100) / 100;        
+//     alert('Your total adoption fees are currently '+adoptFee);
+//     $("#finalAmount").innerHTML = adoptFee = Math.round((adoptFee+ Number.EPSILON) * 100) / 100;
 
 
-
+// dog info function
 function doginfo(name, breed, fee){
     alert('Hi my name is ' +name+ ', I am a ' +breed+ ' and I cost '+fee+' to adopt.');
 }
 
-}
+
+// current error messages when I run the jquery on lines 9 through 13
+// checkout.js:9 Uncaught ReferenceError: $ is not defined
+//     at checkout.js:9
